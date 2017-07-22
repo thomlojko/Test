739a1ca2dd5789036b8694d2e60ee88279125da7
Thomas Lojko <contact@tlogicsofthouse.com>
2017-07-22 17:52:22 -0600

    first commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Thomas Lojko on 2017-07-22.
+//  Copyright © 2017 Thomas Lojko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        print("hello, world!")
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
